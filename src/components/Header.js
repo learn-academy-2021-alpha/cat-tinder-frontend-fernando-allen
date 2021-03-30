f7763cdce8603c9a468783d82a4b7a213ce3b69e
algtan <agtan@ymail.com>
2021-03-30 15:28:25 -0700

    create test directories

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,68 @@
-import React, { Component } from 'react'
-import { Jumbotron, Container } from 'reactstrap'
+import React, { Component, useState } from 'react'
+import {
+  Jumbotron,
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap'
 
-class Header extends Component {
-  render() {
-    return (
-      <>
-        <Jumbotron fluid>
-          <Container fluid>
-            <h1 className="display-3">Super Tinder</h1>
-            <p className="lead">Like Tinder but for superheroes.</p>
-          </Container>
-        </Jumbotron>
-      </>
-    )
-  }
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Superhero Tinder</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">Super Tinder</h1>
+          <p className="lead">Like Tinder but for superheroes.</p>
+        </Container>
+      </Jumbotron>
+    </>
+  )
 }
 export default Header
