9d12fd36b07bdcd01965622bd1cfc93d048f54b9
romerofernando <romero_f22@yahoo.com>
2021-03-31 14:19:29 -0700

    read functionality

--- src/pages/SuperheroShow.js
+++ src/pages/SuperheroShow.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react'
 
 class SuperheroShow extends Component {
   render() {
+    const superhero = this.props.superhero;
+    console.log(superhero)
     return (
       <>
-        <p>Show</p>
+        <div className = "center"> 
+        <h1>{superhero.name}</h1>
+        <br></br>
+        <img src= {superhero.image_url} alt="SuperHero Image" width="25%" height="25%"></img> 
+        <div>Name: {superhero.name}</div>
+        <div>Power: {superhero.power}</div>
+        <div>Hobbies: {superhero.hobbies}</div>
+        <div>Birthplace: {superhero.birthplace}</div>
+        </div>
+       
       </>
     )
   }
